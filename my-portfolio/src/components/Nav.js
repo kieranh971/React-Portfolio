168033236292cf0c01c609e0b9c741aa03687d00
Kieran Harris <kharris19063@gmail.com>
2022-02-24 06:24:05 -0500

    Adding source elements and componenets

---
+++ my-portfolio/src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Nav({ currentPage, setCurrentPage }) {
+    return (
+            <div className="nav-container">
+                <h1>Kieran Harris</h1>
+                <div className="nav-line">
+                    <a href="1" onClick={() => setCurrentPage("AboutMe")} id={currentPage === "AboutMe" ? "active-page" : "nav-anchor"}>About Me</a>
+                    <a href="1" onClick={() => setCurrentPage("MyProjects")} id={currentPage === "MyProjects" ? "active-page" : "nav-anchor"}>My Projects</a>
+                    <a href="1" onClick={() => setCurrentPage("ContactMe")} id={currentPage === "ContactMe" ? "active-page" : "nav-anchor"}>Contact Me</a>
+                    <a href="1" onClick={() => setCurrentPage("Resume")} id={currentPage === "Resume" ? "active-page" : "nav-anchor"}>Resume</a>
+                </div>
+            </div>
+    )
+}
---
+++ my-portfolio/src/components/Portfolio.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import AboutMe from "./pages/AboutMe";
+import MyProjects from "./pages/MyProjects";
+import ContactMe from "./pages/ContactMe";
+import Resume from "./pages/Resume";
+import Nav from "./Nav";
+
+function Portfolio() {
+
+    return (
+        <div className="container">
+            <p> This is a test to render the portfolio page </p>
+        </div>
+    );
+
+
+}
+
+export default Portfolio
